3791f0e8cd78313f46db92e99f12e97ca1778ad5
Andrew Chen <aychen314159@gmail.com>
2017-06-24 03:47:01 +0000

    Setup file

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+
 gpio unexportall
 gpio export 2 out
 gpio export 11 out
-gpio export 8 in
-gpio export 9 in
 gpio export 18 in
 gpio export 23 in
 gpio export 24 in
@@ -20,3 +19,6 @@ gpio export 4 in
 gpio export 17 in
 gpio export 27 in
 gpio export 22 in
+gpio export 9 in
+gpio export 8 in
+echo Export complete.
