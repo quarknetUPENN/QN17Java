b61496012f082a761cbab37adcd0d5e930ecd4a2
Andrew Chen <aychen314159@gmail.com>
2017-06-28 17:00:57 -0400

    added src

---
+++ src/com/github/QuarknetPenn/QN17Java/Main.java
@@ -0,0 +1,13 @@
+package com.github.QuarknetPenn.QN17Java;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        int i = 2;
+        int j = 3;
+        System.out.println(i+j);
+    }
+}
