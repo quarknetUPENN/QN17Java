65ae573b93de26c498ce06f970de61c9330e0c00
Andrew Chen <aychen314159@gmail.com>
2017-06-28 17:52:15 -0400

    blargh

--- src/com/github/QuarknetPenn/QN17Java/Main.java
+++ src/com/github/QuarknetPenn/QN17Java/Main.java
@@ -2,12 +2,30 @@ package com.github.QuarknetPenn.QN17Java;
 
 public class Main
 {
-
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
-        int i = 2;
-        int j = 3;
-        System.out.println(i+j);
+        while(1){
+
+        }
+    }
+
+    static enum FPGAPINS{
+        ENABLE,
+        VALID,
+        EMPTY,
+        CLOCK,
+        LEVEL_0,
+        LEVEL_1,
+        LEVEL_2,
+        LEVEL_3,
+        SUBLEVEL,
+        TUBENUM_0,
+        TUBERAD_1,
+        TUBERAD_2,
+        TUBERAD_3,
+        TUBERAD_4,
+        TUBERAD_5,
+        TUBERAD_6,
+        TUBERAD_7;
     }
 }
